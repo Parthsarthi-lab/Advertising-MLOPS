811ec89511e499c6db09a86b97d3bd34e7d78572
Parthsarthi Joshi <parthucmas@gmail.com>
2024-12-08 12:00:05 +0530

    Data validation component successful

--- training/components/common/data_ingestion.py
+++ training/components/common/data_ingestion.py
@@ -24,7 +24,7 @@ class DataIngestion:
 
     def save_data(self):
         try:
-
+            info_logger.info("Data Ingestion Component started")
             status = False
             data_file_path = os.path.join(self.config.data_dir, "advertising_data.csv")
 
@@ -35,7 +35,7 @@ class DataIngestion:
                 with open(self.config.STATUS_FILE, "w") as f:
                     f.write(f"Data Ingestion status: {status}")
 
-                info_logger.info(f"Data Ingestion completed successfully")
+            info_logger.info(f"Data Ingestion Component completed")
         except Exception as e:
             with open(self.config.STATUS_FILE, "w") as f:
                 f.write(f"Data Ingestion status: {status}")
